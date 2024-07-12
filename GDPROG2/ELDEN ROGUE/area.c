d1e67a9f358d07f9c886415cf341db99d90db08c
nicolo81 <nicolo_pacannuayan@dlsu.edu.ph>
2024-07-12 20:11:48 +0800

    can now move in stormveil

--- GDPROG2/ELDEN ROGUE/area.c
+++ GDPROG2/ELDEN ROGUE/area.c
@@ -2,27 +2,44 @@
 #include "structures.h"
 
 // EMPTY TILE = 0
-// PLAYER TILE = 1
+// PLAYER TILE = [SELECTED TILE] + 1
 // SPAWN TILE = 2
-// BOSS TILE = 3
-// DOOR TILE = 4
-// FAST TRAVEL = 5
+// BOSS TILE = 4
+// DOOR TILE = 6
+// FAST TRAVEL = 8
 
 void runStormveil()
 {
      Stormveil sFloor; 
-     int aTemp[21] = {0,4,0,2,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0};
+     int aTemp[21] = {0,6,0,2,0,2,0,0,0,0,0,0,0,0,0,0,0,0,0,9,0};
      char cInput;
+
+     initializeFloor(aTemp, 21, &sFloor, 7, 3);
 
      do{
 
-     initializeFloor(aTemp, 21, &sFloor, 7, 3);
+     // initializeFloor(aTemp, 21, &sFloor, 7, 3);
      printFloor(sFloor, 7, 3);
      printf("\n[INPUT]: ");
      scanf(" %c", &cInput);
-     // processInput(cInput, &sFloor, 7, 3);
+     processInput(cInput, &sFloor, 7, 3);
+
+     for(int i = 0; i < 7; i++){
+          printf("\n");
+          for(int j = 0; j < 3; j++){
+               printf("%d ", sFloor.aSpawnFloor[i][j]);
+          }
+     }
+     printf("\n");
 
      } while(cInput != '0');
+
+     // for(int i = 0; i < 7; i++){
+     //      printf("\n");
+     //      for(int j = 0; j < 3; j++){
+     //           printf("%d ", sFloor.aSpawnFloor[i][j]);
+     //      }
+     // }
 
 }
 
@@ -51,8 +68,8 @@ void printFloor(Stormveil sFloor, int nRow, int nColumn){
                     printf("┌───┐");
                     printf("\x1b[0m");
                }
-               else if(sFloor.aSpawnFloor[i][j] == 1){
-                    printf("\x1b[38;5;34m");
+               else if(sFloor.aSpawnFloor[i][j] == 1 || sFloor.aSpawnFloor[i][j] == 3 || sFloor.aSpawnFloor[i][j] == 5 || sFloor.aSpawnFloor[i][j] == 7 || sFloor.aSpawnFloor[i][j] == 9){
+                    printf("\x1b[38;5;13m");
                     printf("╔═══╗");
                     printf("\x1b[0m");
                }
@@ -61,11 +78,16 @@ void printFloor(Stormveil sFloor, int nRow, int nColumn){
                          printf("┌───┐");
                          printf("\x1b[0m");
                     }
-                         else if(sFloor.aSpawnFloor[i][j] == 4){
+                         else if(sFloor.aSpawnFloor[i][j] == 6){
                               printf("\x1b[38;5;220m");
                               printf("┌───┐");
                               printf("\x1b[0m");
                          }
+                              else if(sFloor.aSpawnFloor[i][j] == 8){
+                                   printf("\x1b[38;5;34m");
+                                   printf("┌───┐");
+                                   printf("\x1b[0m");
+                              }
           }
 
           printf("\n");
@@ -77,8 +99,8 @@ void printFloor(Stormveil sFloor, int nRow, int nColumn){
                     printf("│   │");
                     printf("\x1b[0m");
                }
-               else if(sFloor.aSpawnFloor[i][j] == 1){
-                    printf("\x1b[38;5;34m");
+               else if(sFloor.aSpawnFloor[i][j] == 1 || sFloor.aSpawnFloor[i][j] == 3 || sFloor.aSpawnFloor[i][j] == 5 || sFloor.aSpawnFloor[i][j] == 7 || sFloor.aSpawnFloor[i][j] == 9){
+                    printf("\x1b[38;5;13m");
                     printf("║   ║");
                     printf("\x1b[0m");
                }
@@ -87,11 +109,16 @@ void printFloor(Stormveil sFloor, int nRow, int nColumn){
                          printf("│   │");
                          printf("\x1b[0m");
                     }
-                         else if(sFloor.aSpawnFloor[i][j] == 4){
+                         else if(sFloor.aSpawnFloor[i][j] == 6){
                               printf("\x1b[38;5;220m");
                               printf("│   │");
                               printf("\x1b[0m");
                          }
+                              else if(sFloor.aSpawnFloor[i][j] == 8){
+                                   printf("\x1b[38;5;34m");
+                                   printf("│   │");
+                                   printf("\x1b[0m");
+                              }
           }
 
           printf("\n");
@@ -103,8 +130,8 @@ void printFloor(Stormveil sFloor, int nRow, int nColumn){
                     printf("└───┘");
                     printf("\x1b[0m");
                }
-               else if(sFloor.aSpawnFloor[i][j] == 1){
-                    printf("\x1b[38;5;34m");
+               else if(sFloor.aSpawnFloor[i][j] == 1 || sFloor.aSpawnFloor[i][j] == 3 || sFloor.aSpawnFloor[i][j] == 5 || sFloor.aSpawnFloor[i][j] == 7 || sFloor.aSpawnFloor[i][j] == 9){
+                    printf("\x1b[38;5;13m");
                     printf("╚═══╝");
                     printf("\x1b[0m");
                }
@@ -113,11 +140,16 @@ void printFloor(Stormveil sFloor, int nRow, int nColumn){
                          printf("└───┘");
                          printf("\x1b[0m");
                     }
-                         else if(sFloor.aSpawnFloor[i][j] == 4){
+                         else if(sFloor.aSpawnFloor[i][j] == 6){
                               printf("\x1b[38;5;220m");
                               printf("└───┘");
                               printf("\x1b[0m");
                          }
+                              else if(sFloor.aSpawnFloor[i][j] == 8){
+                                   printf("\x1b[38;5;34m");
+                                   printf("└───┘");
+                                   printf("\x1b[0m");
+                              }
           }
 
           printf("\n");
@@ -126,18 +158,43 @@ void printFloor(Stormveil sFloor, int nRow, int nColumn){
 
 }
 
-// void processInput(char cInput, Stormveil* sFloor, int nRow, int nColumn){
-//      // printf("Hello\n");
+void processInput(char cInput, Stormveil* sFloor, int nRow, int nColumn){
+     // printf("Hello\n");
 
-//      for(int i = 0; i < nRow; i++){
-//           for(int j = 0; j < nColumn; j++){
-//                if(sFloor->aSpawnFloor[i][j] == 1){
-//                     switch(cInput){
-//                          case 'W': 
-//                               sFloor->aSpawnFloor[i][j] = 0
-//                     }
-//                }
-//           }
-//      }
+     for(int i = 0; i < nRow; i++){
+          for(int j = 0; j < nColumn; j++){
+               if(sFloor->aSpawnFloor[i][j] == 1 || sFloor->aSpawnFloor[i][j] == 3 || sFloor->aSpawnFloor[i][j] == 5 || sFloor->aSpawnFloor[i][j] == 7 || sFloor->aSpawnFloor[i][j] == 9){
+                    switch(cInput){
+                         case 'W': 
+                              if(i == 0)
+                              break;
+                              sFloor->aSpawnFloor[i][j]--;
+                              sFloor->aSpawnFloor[i-1][j]++;
+                              break;
+                         case 'D':
+                              if(j == 2)
+                              break;
+                              sFloor->aSpawnFloor[i][j]--;
+                              j++;
+                              sFloor->aSpawnFloor[i][j]++;
+                              break;
+                         case 'A':
+                              if(j == 0)
+                              break;
+                              sFloor->aSpawnFloor[i][j]--;
+                              j--;
+                              sFloor->aSpawnFloor[i][j]++;
+                              break;
+                         case 'S':
+                              if(i == 6)
+                              break;
+                              sFloor->aSpawnFloor[i][j]--;
+                              i++;
+                              sFloor->aSpawnFloor[i][j]++;
+                              break;
+                    }
+               }
+          }
+     }
 
-// }
+}
--- GDPROG2/ELDEN ROGUE/area.h
+++ GDPROG2/ELDEN ROGUE/area.h
@@ -5,4 +5,4 @@
 void runStormveil();
 void initializeFloor(int* pTemp, int nSize, Stormveil* sFloor, int nRow, int nColumn);
 void printFloor(Stormveil sFloor, int nRow, int nColumn);
-// void processInput(char cInput, Stormveil* sFloor, int nRow, int nColumn);
+void processInput(char cInput, Stormveil* sFloor, int nRow, int nColumn);
