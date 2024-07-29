d03d275958bd1a757393436076ee4b14717f27ff
nicolo81 <nicolo_pacannuayan@dlsu.edu.ph>
2024-07-29 16:39:37 +0800

    fast travel

--- GDPROG2/ELDEN ROGUE/area.h
+++ GDPROG2/ELDEN ROGUE/area.h
@@ -4,10 +4,10 @@
 #include "character_creation.h"
 #include "title.h"
 
-void runArea(Area* pArea, Array sCoordinate, Player* pPlayer);
+void runArea(Area* pArea, Array sCoordinate, Player* pPlayer, int nShard);
 void areaScreen(char* strAreaName);
 void printFloor(Area* pArea, Array sCoordinate);
-void processInput(char cInput, Area* pArea, Array sCoordinate, Player* pPlayer);
-void floorPass(Player* pPlayer, Area* pArea, int* pPlayerLocation);
+void processInput(char cInput, Area* pArea, Array sCoordinate, Player* pPlayer, int nShard);
+void floorPass(Player* pPlayer, Area* pArea, int* pPlayerLocation, int nShard);
 void initializeArea(int nRow, int nColumn, int nRowOffset, Area* pArea, int* pTemp);
 void areaSelect(char cAreaIndex, char cFastTravelTile, Player* pPlayer);
